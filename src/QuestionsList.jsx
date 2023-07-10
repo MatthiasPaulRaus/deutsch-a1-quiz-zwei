dcdfb65522b6d2a6f712b5fa1dd5bc6f1c0e733e
matthias paul raus <81903294+MatthiasPaulRaus@users.noreply.github.com>
2023-07-10 13:21:12 +0200

    first commit

---
+++ src/QuestionsList.jsx
@@ -0,0 +1,221 @@
+const QuestionsList = [
+    {
+        question:"Entschuldigung! Wir suchen das Parkhaus.",
+        answersList: [
+            {answer:"Fahren Sie geradeaus und dann nach 500 Metern links.", isCorrect:true},
+            {answer:"Tut mir leid, dass ich kein Auto habe.", isCorrect:false},
+            {answer:"Sie können mir auch nicht helfen.", isCorrect:false},
+        ]
+    },{
+        question:"Entschuldigung! Können Sie mir helfen?",
+        answersList: [
+            {answer:"Hm, ich habe leider keine Ahnung.", isCorrect:false},
+            {answer:"Ja klar, was kann ich für Sie tun?", isCorrect:true},
+            {answer:"Warum soll ich Ihnen helfen?", isCorrect:false},
+        ]
+    },{
+        question:"Wo ist denn hier der Marktplatz?",
+        answersList: [
+            {answer:"Gehen Sie zurück zum Parkhaus und da ist der Wochenmarkt.", isCorrect:false},
+            {answer:"Gegenüber vom Rathaus war früher ein großer Platz.", isCorrect:false},
+            {answer:"Gehen Sie einfach geradeaus, dann sehen Sie ihn gleich.", isCorrect:true},
+        ]
+    },{
+        question:"Darf ich Sie etwas fragen?",
+        answersList: [
+            {answer:"Gern! Was wollen Sie denn wissen?", isCorrect:true},
+            {answer:"Lieber nicht, ich bin auch fremd hier.", isCorrect:false},
+            {answer:"Nein! Alle wollen mich etwas fragen.", isCorrect:false},
+        ]
+    },{
+        question:"Hallo! Ist es noch weit bis zum Museum?",
+        answersList: [
+            {answer:"Sie sollten eine kleine Pause machen.", isCorrect:false},
+            {answer:"Nein, das ist gleich hier in der Nähe.", isCorrect:true},
+            {answer:"Ja, das kann ich Ihnen ganz genau sagen.", isCorrect:false},
+        ]
+    },{
+        question:"In diesem Raum steht ein großes Bett und ein Schrank.",
+        answersList: [
+            {answer:"Das kann ein Wohnzimmer sein.", isCorrect:false},
+            {answer:"Haben Sie auch ein Kinderzimmer?", isCorrect:false},
+            {answer:"Ist das Ihr Schlafzimmer?", isCorrect:true},
+        ]
+    },{
+        question:"Die Wohnung ist im zweiten Stock.",
+        answersList: [
+            {answer:"Ja gut, aber hat sie auch einen Balkon?", isCorrect:true},
+            {answer:"Und wie viele Nachbarn wohnen in der Wohnung?", isCorrect:false},
+            {answer:"Die Garage hat leider keinen Balkon.", isCorrect:false},
+        ]
+    },{
+        question:"Ich bin am Samstag umgezogen.",
+        answersList: [
+            {answer:"Ich kann dir gerne helfen.", isCorrect:false},
+            {answer:"Und bist du zufrieden mit der neuen Wohnung?", isCorrect:true},
+            {answer:"Tut mir leid, aber ich bin auch neu hier.", isCorrect:false},
+        ]
+    },{
+        question:"Wie findest du seine neue Wohnung?",
+        answersList: [
+            {answer:"Das Haus ist schön, aber er ist langweilig.", isCorrect:false},
+            {answer:"Der Garten neben dem Haus ist ziemlich groß.", isCorrect:false},
+            {answer:"Sein Wohnzimmer mit Balkon mag ich besonders.", isCorrect:true},
+        ]
+    },{
+        question:"Leider ist die Miete für mich ziemlich hoch.",
+        answersList: [
+            {answer:"Wie viel kassiert der Vermieter denn pro Monat?", isCorrect:true},
+            {answer:"Das ist doch ein tolles Angebot.", isCorrect:false},
+            {answer:"Bezahlt ihr zusammen oder getrennt?", isCorrect:false},
+        ]
+    },
+    
+    {
+        question:"Wie finden Sie es hier in der Stadt?",
+        answersList: [
+            {answer:"Hier gefällt es mir sehr gut.", isCorrect:true},
+            {answer:"Mir gefallen die Wanderwege besonders gut.", isCorrect:false},
+            {answer:"Wir lieben die frische Landluft.", isCorrect:false},
+        ]
+    },{
+        question:"Warum wollen Sie die Wohnung mieten?",
+        answersList: [
+            {answer:"Sie liegt außerhalb und hat keine Garage.", isCorrect:false},
+            {answer:"Sie liegt im Zentrum und ist nicht zu teuer.", isCorrect:true},
+            {answer:"Sie ist direkt neben dem Bahnhof und ist zu teuer.", isCorrect:false},
+        ]
+    },{
+        question:"Wie finden Sie die Kneipen in der Altstadt?",
+        answersList: [
+            {answer:"Sie liegen außerhalb des Zentrums und sind langweilig.", isCorrect:false},
+            {answer:"Da kann man immer viele Touristen finden.", isCorrect:false},
+            {answer:"Dort gibt es immer viele interessante Leute.", isCorrect:true},
+        ]
+    },{
+        question:"Was kann man in diesem Stadtviertel gut machen?",
+        answersList: [
+            {answer:"Hier kann man gut einkaufen und ausgehen.", isCorrect:true},
+            {answer:"Hier gibt es keine Geschäfte und keine Restaurants.", isCorrect:false},
+            {answer:"Hier finden Sie keinen Parkplatz.", isCorrect:false},
+        ]
+    },{
+        question:"Gibt es hier viele Sehenswürdigkeiten?",
+        answersList: [
+            {answer:"Was wollen Sie denn sehen?", isCorrect:false},
+            {answer:"Ja, vor allem in der Altstadt.", isCorrect:true},
+            {answer:"Im Zentrum gibt es viele Kneipen.", isCorrect:false},
+        ]
+    },{
+        question:"Wollen wir am Freitag zusammen ins Kino gehen?",
+        answersList: [
+            {answer:"Passt dir das Kino?", isCorrect:false},
+            {answer:"Kannst du den Freitag verschieben?", isCorrect:false},
+            {answer:"Passt es dir um 20 Uhr?", isCorrect:true},
+        ]
+    },{
+        question:"Fabian hat seit zwei Tagen Ohrenschmerzen.",
+        answersList: [
+            {answer:"Seine Freundin sagt, er soll zum Hausarzt gehen.", isCorrect:true},
+            {answer:"Jana meint, er will zum Arzt gehen.", isCorrect:false},
+            {answer:"Katja glaubt, er kann nicht gehen.", isCorrect:false},
+        ]
+    },{
+        question:"Herr Doktor, ich brauche Ihren Rat.",
+        answersList: [
+            {answer:"Dann gehen Sie am besten ins Rathaus.", isCorrect:false},
+            {answer:"Ja gern, was fehlt Ihnen denn?", isCorrect:true},
+            {answer:"Ich kann dir keine Tipps geben.", isCorrect:false},
+        ]
+    },{
+        question:"Welche Pläne hast du für die Zukunft?",
+        answersList: [
+            {answer:"Leider kann ich mich nicht daran erinnern.", isCorrect:false},
+            {answer:"Nächste Woche werde ich wieder gesund sein.", isCorrect:false},
+            {answer:"Ich will unbedingt im Ausland studieren.", isCorrect:true},
+        ]
+    },{
+        question:"Hattet ihr einen schönen Urlaub?",
+        answersList: [
+            {answer:"Ja, wir haben uns wunderbar erholt.", isCorrect:true},
+            {answer:"Nein, wir bleiben dieses Jahr zuhause.", isCorrect:false},
+            {answer:"Leider ist der Ausflug ins Wasser gefallen.", isCorrect:false},
+        ]
+    },
+    
+
+    {
+        question:"Mein Fahrrad ist kaputt.",
+        answersList: [
+            {answer:"Kein Problem. Ich kümmere mich darum.", isCorrect:true},
+            {answer:"Oh je, heute klappt es nicht mehr.", isCorrect:false},
+            {answer:"Hm, mach dir keine Sorgen.", isCorrect:false},
+        ]
+    },{
+        question:"Wie oft machen Sie Sport?",
+        answersList: [
+            {answer:"Montags von 18 bis 19:30 Uhr.", isCorrect:false},
+            {answer:"Dreimal pro Woche.", isCorrect:true},
+            {answer:"Mindestens 90 Minuten.", isCorrect:false},
+        ]
+    },{
+        question:"Wann machen Sie Urlaub?",
+        answersList: [
+            {answer:"Zwei Wochen, vielleicht auch länger.", isCorrect:false},
+            {answer:"Im Juli habe ich frei.", isCorrect:false},
+            {answer:"Vom 15. Juli bis zum 1. August.", isCorrect:true},
+        ]
+    },{
+        question:"Ich habe meine Prüfung bestanden.",
+        answersList: [
+            {answer:"Das würde ich gerne mit dir feiern.", isCorrect:true},
+            {answer:"Alles Gute und viel Erfolg!", isCorrect:false},
+            {answer:"Wir organisieren eine Überraschungsparty für dich.", isCorrect:false},
+        ]
+    },{
+        question:"Das Wetter war wunderbar am Wochenende.",
+        answersList: [
+            {answer:"Es gab ein starkes Wärmegewitter am Abend.", isCorrect:false},
+            {answer:"Wir mussten den Ausflug absagen.", isCorrect:false},
+            {answer:"Wir konnten sogar im See baden.", isCorrect:true},
+        ]
+    },{
+        question:"Ich hatte heute einen schlechten Tag.",
+        answersList: [
+            {answer:"Ach komm! Was ist denn passiert?", isCorrect:true},
+            {answer:"Kein Problem! Wann ist das passiert?", isCorrect:false},
+            {answer:"Ja, ich habe es gleich erkannt.", isCorrect:false},
+        ]
+    },{
+        question:"Bitte räum endlich dein Zimmer auf!",
+        answersList: [
+            {answer:"Ich muss immer machen, was ich will.", isCorrect:false},
+            {answer:"Immer soll ich machen, was du sagst.", isCorrect:true},
+            {answer:"Ich will aber nicht, dass du das machst.", isCorrect:false},
+        ]
+    },{
+        question:"Musst du wirklich schon gehen?",
+        answersList: [
+            {answer:"Es war sehr angenehm hier, vielen Dank für die Einladung.", isCorrect:false},
+            {answer:"Tut mir leid. Das nächste Mal bin ich pünktlich.", isCorrect:false},
+            {answer:"Der Abend war sehr schön, aber ich muss jetzt gehen.", isCorrect:true},
+        ]
+    },{
+        question:"Wie gesund lebst du?",
+        answersList: [
+            {answer:"Ich esse viel Gemüse und mache Sport.", isCorrect:true},
+            {answer:"Ich schlafe maximal 5 Stunden und arbeite viel.", isCorrect:false},
+            {answer:"Ich esse und trinke viel und mache gern Videospiele.", isCorrect:false},
+        ]
+    },{
+        question:"Welches Sofa findest du am besten.",
+        answersList: [
+            {answer:"Das graue da hinten finde ich praktisch.", isCorrect:false},
+            {answer:"Das blaue hier ist wirklich günstig.", isCorrect:false},
+            {answer:"Das braune Ledersofa ist am bequemsten.", isCorrect:true},
+        ]
+    },
+
+].sort(() => Math.random() - 0.5);
+
+export default QuestionsList;
